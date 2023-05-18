807b01023a23832e9f5d12f72aa77a5ae4ed56c5
Maylencasal <108495714+Maylencasal@users.noreply.github.com>
2023-05-18 19:43:27 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    // Seed the random number generator
+    srand(static_cast<unsigned int>(time(0)));
+
+    // Generate a random number between 1 and 100
+    int secretNumber = rand() % 100 + 1;
+
+    int guess;
+    int attempts = 0;
+
+    std::cout << "Welcome to Guess the Number!\n";
+
+    // Game loop
+    while (true) {
+        std::cout << "Enter your guess (1-100): ";
+        std::cin >> guess;
+
+        attempts++;
+
+        if (guess == secretNumber) {
+            std::cout << "Congratulations! You guessed the number in " << attempts << " attempts.\n";
+            break;
+        } else if (guess < secretNumber) {
+            std::cout << "Too low! Try again.\n";
+        } else {
+            std::cout << "Too high! Try again.\n";
+        }
+    }
+
+    return 0;
+}
